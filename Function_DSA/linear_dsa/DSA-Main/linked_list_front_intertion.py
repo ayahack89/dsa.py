97c0d4e9076a6a35ebfcc7378d80bb4a7d10a691
Ayanabha Chatterjee <ayanabhachatterjee@gmail.com>
2023-03-17 00:59:35 +0530

    File transfer

---
+++ Function_DSA/linear_dsa/DSA-Main/linked_list_front_intertion.py
@@ -0,0 +1,41 @@
+#Intertion at the front of a linked list.
+# Conditions :-
+#       1) Secure your previous head value.
+#       2) Create a newNode.
+#       3) Include newNode at Head position.
+#       4) Now link the newNode's address part to the last head node (temp).
+ 
+class List:
+    def __init__(self,num):
+        self.data=num
+        self.address=None
+class linkedList:
+    def __init__(self):
+        self.head=None
+    def create(self,num):
+        newNode=List(num)
+        if self.head is None:
+            self.head=newNode
+            self.last=newNode
+        else:
+            self.last.address=newNode
+            self.last=newNode
+
+#Insert the front value.
+    def insert_front(self,num):
+        temp=self.head
+        newNode=List(num)
+        self.head=newNode
+        newNode.address=temp
+
+    def printList(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.data)
+            temp=temp.address
+
+oop=linkedList()
+oop.create(int(input("Enter a number: ")))
+oop.insert_front(int(input("Enter the number that you insert at the front position: ")))
+print("Output:-")
+oop.printList()
