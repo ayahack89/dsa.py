f647d0bdaee17f19d0f47d4dae7243064b899aae
Ayanabha Chatterjee <ayanabhachatterjee@gmail.com>
2023-03-20 20:07:18 +0530

    code

--- Function_DSA/linear_dsa/Linked_list/linked_list_delete_of_a_given_node.py
+++ Function_DSA/linear_dsa/Linked_list/linked_list_delete_of_a_given_node.py
@@ -18,3 +18,31 @@ class linkedList:
         else:
             self.last.address=newNode
             self.last=newNode
+    def delete_position(self,position):
+        position = position-1
+        delete_node=self.head
+        while (position>0):
+            position=position-1
+            delete_node=delete_node.address
+
+        if self.head is None:
+            print("The list is empty.")
+            return print("Error")
+        else:
+            self.head=self.head.address
+            delete_node.address=None
+            return delete_node.data
+    def printList(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.data)
+            temp=temp.address
+
+o=linkedList()
+a=int(input("Number of inputs:- "))
+for j in range(0,a):
+    o.create(int(input("Node.data: ")))
+position=int(input("Input a random position: "))
+o.delete_position(position)
+o.printList()
+        
