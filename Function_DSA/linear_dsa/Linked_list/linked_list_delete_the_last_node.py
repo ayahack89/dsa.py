71c30c84d3c275dd72c7fc50582bc69a9441b169
Ayanabha Chatterjee <ayanabhachatterjee@gmail.com>
2023-03-20 21:48:01 +0530

    new

---
+++ Function_DSA/linear_dsa/Linked_list/linked_list_delete_the_last_node.py
@@ -0,0 +1,40 @@
+#Linked List delete the last node.
+# Structure of a Linked list look like:-
+                 # NodeA->NodeB->NodeC->NodeD->NodeE
+class List:
+    def __init__(self,num):
+        self.data=num
+        self.address=None
+class linkedList:
+    def __init__(self):
+        self.head=None
+    def create(self,num):
+        newNode=List(num)
+        if self.head is None:
+            self.head=newNode
+            self.last=newNode
+        else:
+            self.last.address=newNode
+            self.last=newNode
+    def delete_lastNode(self):
+        delete_node=self.head
+        while delete_node is not self.head:
+            previous=delete_node
+            delete_node=delete_node.address
+        previous.address=None
+        print(previous.data)
+        self.last=previous
+    def printList(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.data)
+            temp=temp.address
+
+#Call the functions.
+o=linkedList()
+a=int(input("Number of inputs:-  "))
+for j in range(0,a):
+    o.create(int(input("Node.data: ")))
+o.delete_lastNode()
+o.printList()
+# CODE ERROR , UNDER CONSTRACTION........
