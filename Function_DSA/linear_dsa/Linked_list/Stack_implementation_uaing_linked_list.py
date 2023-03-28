8882430e2523521dbe7304e5a3cd1cde0d7f0627
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-03-28 20:27:28 +0530

    Code update

--- Function_DSA/linear_dsa/Linked_list/Stack_implementation_uaing_linked_list.py
+++ Function_DSA/linear_dsa/Linked_list/Stack_implementation_uaing_linked_list.py
@@ -9,42 +9,36 @@ class node:
         self.address=None
 class Stack:
     def __init__(self):
-        self.head=None
-    def create(self,data):
-        newNode=node(data)
-        if self.head is None:
-            self.head=newNode
-            self.last=newNode
-        else:
-            self.last.adddress=newNode
-            self.last=newNode
-# Now we implement the Linked List with stack concept....
+        self.top=None
     def push(self,data):
         newNode=node(data)
-        self.last.address=newNode
-        self.last=newNode
+        if self.top is None:
+            self.top=newNode
+            self.last=newNode
+        else:
+            newNode.address=self.top
+            self.top=newNode
     def pop(self):
-        delete_node=self.head
+        if self.top is None:
+            print("Linked List is empty, can't delete it.")
+            return print("ERROR")
         if self.head is None:
             print("Linked List is empty , can't delete its.")
             return print("ERROR.")
         else:
-            self.head=delete_node.address
+            self.top=self.top.address
 
-
-# Call...
+#Call....
 co=Stack()
-a=int(input("Number of inputs:- "))
-for j in range(0,a):
-    co.create(int(input("Node.data: ")))
-choice=str(input(" Push->1 \n Pop->2 \n Enter your choice: "))
+choice=str(input(" Push->1 \n Pop->2 \n Enter your choice:"))
 if (choice=='1'):
-    data=int(input("Insert a element-> "))
-    co.push(data)
-    print("Data inserted...")
+    a=int(input("Number of Inputs:- "))
+    for j in range(0,a):
+        data=int(input("Node.Data: "))
+        co.push(data)
+        print("Data Inserted.....")
 elif (choice=='2'):
     co.pop()
-    print("Data deleted...")
+    print("Data deleted.....")
 else:
     print("ERROR")
-    
