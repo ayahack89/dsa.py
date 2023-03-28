3328ae69d6dd325f6e1713ddf0f03358bf72f56b
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-03-28 23:38:21 +0530

    Updata New Searching

--- Searching-DSA/interpolation_searching.py
+++ Searching-DSA/interpolation_searching.py
@@ -1,3 +1,20 @@
 #Interpolation searching in python.
-import 
+import numpy as np
 x=[1,2,4,6,8,10,12,15,18,20]
+array=np.array(x)
+low=0
+high=len(array)-1
+search=int(input("Enter an Element to search-> "))
+found=0
+while(low<=high):
+    formula=round(low+((search-x[low])/(x[high]-x[low]))*(high-low))
+    if (array[formula]==search):
+        found=1
+        print(f"Element found at {formula}")
+        break
+    elif (array[formula]>search):
+        high=formula-1
+    elif (array[formula]<search):
+       low=formula+1
+else:
+    print("Element not found!") 
