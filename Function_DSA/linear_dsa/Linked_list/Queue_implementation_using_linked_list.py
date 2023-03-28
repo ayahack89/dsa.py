f55f2ec29f7da3169ae4d9410c68ce77bcab1216
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-03-28 21:39:12 +0530

    Code Updata

--- Function_DSA/linear_dsa/Linked_list/Queue_implementation_using_linked_list.py
+++ Function_DSA/linear_dsa/Linked_list/Queue_implementation_using_linked_list.py
@@ -9,43 +9,46 @@ class node:
         self.address=None
 class Queue:
     def __init__(self):
-        self.head=None
-    def create(self,data):
+        self.front=None
+        self.rear=None
+    def Insert(self,data):
         newNode=node(data)
-        if self.head is None:
-            self.head=newNode
-            self.last=newNode
+        if self.front is None:
+            self.front=newNode
+            self.rear=newNode
         else:
-            self.last.address=newNode
-            self.last=newNode
+            self.rear.address=newNode
+            self.rear=newNode
 # Now we implement the Linked List with queue concept..... 
-    def rear(self,data):
-        newNode=node(data)
-        if self.head is not None:
-            return print("FULL")
+    def delete(self):
+        if self.front is None:
+            print("Empty...")
+            return print("Underflow!")
         else:
-            self.head=newNode.address
-    def front(self):
-        delete_node=self.head
-        if self.head is None:
-            print("Linked list is empty , we can't delete it.")
-            return print("ERROR")
-        else:
-            self.head=delete_node.address
+            print(self.front.data)
+            self.front=self.front.address
+    def printnode(self):
+        temp=self.front
+        while temp is not None:
+            print(temp.data)
+            temp=temp.address
 
 #Call....
 oc=Queue()
-b=int(input("Number of inputs:- "))
-for k in range(0,b):
-    oc.create(int(input("Node.data: ")))
-choice=str(input(" Insert->1 \n Delete->2 \n Enter you choice:"))
-if (choice=='1'):
-    data=int(input("Insert an element: "))
-    oc.rear(data)
-    print("Data Inserted...")
-elif (choice=='2'):
-    oc.front()
-    print("Data deleted...")
-else:
-    print("ERROR")
+while True:
+    choice=str(input("Insert->1 \n Delete->2 \n Enter your choice: "))
+    if (choice=='1'):
+        b=int(input("Number of Inputs:-"))
+        for k in range(0,b):
+            data=int(input("Node.Data: "))
+            oc.Insert(data)
+            oc.printnode()
+            print("Data Inserted.....")
+    elif (choice=='2'):
+        oc.delete()
+        oc.printnode()
+        print("Data Deleted.....")
+
+    else:
+        print("ERROR")
 
