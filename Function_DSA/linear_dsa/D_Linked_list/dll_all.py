7876a5dad8f524049c49e637d49782a2999829fe
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-04-03 21:37:31 +0530

    New Update

--- Function_DSA/linear_dsa/D_Linked_list/dll_all.py
+++ Function_DSA/linear_dsa/D_Linked_list/dll_all.py
@@ -12,14 +12,31 @@ class D_linked_L:
         newNode=List(data)
         if self.head is None:
             self.head=newNode
-            self.Last=newNode
+            self.last=newNode
         else:
             newNode.previous=self.last
             self.last.next=newNode
             self.last=newNode
     def insert_at_front_position(self,data):
         temp=self.head
-        
+        newNOde=List(data)
+        self.head=newNOde.next
+        newNOde.next=temp
+    def insert_at_random_position(self,data,position):
+        temp=self.head
+        newNode=List(data)
+        position=position-2
+        while(position>0):
+            position=position-1
+            temp=temp.next
+        newNode.previous=temp.next
+        newNode.next=temp
+    def insert_at_last_position(self,data):
+        temp=self.head
+        newNode=List(data)
+        self.last.next=newNode
+        newNode.previous=self.last
+
           
     def printList(self):
         temp1=self.head
@@ -36,5 +53,20 @@ class D_linked_L:
 
 
 #Call.........
-
+p=D_linked_L()
+a=int(input("Number of inputs: "))
+for j in range(0,a):
+    p.create(int(input("NOde.Data > ")))
+data=int(input("Enter a data to insert: "))
+position=int(input("Enter a position to insert a given node: "))
+p.insert_at_front_position(data)
+p.printList()
+print("Data Inserted.....")
+p.insert_at_random_position(data,position)
+p.printList()
+print("Data Inserted......")
+p.insert_at_last_position(data)
+p.printList()
+print("Data Inserted......")
+print("END")
      
