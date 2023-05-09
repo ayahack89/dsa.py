07b43e76882aaf5964349d9bd49d4f26c31dd6ca
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2023-05-09 20:13:14 +0530

    Code Update
    Element change

--- DSA/Linear/Static/arrays/Basic-Array-Using-lenght.py
+++ DSA/Linear/Static/arrays/Basic-Array-Using-lenght.py
@@ -1,7 +1,5 @@
 # Array counting the length function.
-
-
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+a = [1, 21, 4, 5, 10, 7, 8, 9]
 print(f":- {a}")
 for i in a:
-    print(f"INDEX:- {i} DATA:- {a[i]}")
+    print(f"INDEX:- {a.index(i)} DATA:- {i}")
